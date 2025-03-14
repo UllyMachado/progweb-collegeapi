5fdbcaa03068200392fdd62dfc9ab521083e5d5e
Ully <ullyvieira@hotmail.com>
2025-03-14 20:16:12 -0300

    First Commit

---
+++ src/main/java/progweb/collegeapi/CollegeapiApplication.java
@@ -0,0 +1,13 @@
+package progweb.collegeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeapiApplication.class, args);
+	}
+
+}
